3263393a3535c712f0e90a2ee6804852f56d689e
Kseniia Liepokhina <kseniya.lepiohina@gmail.com>
2026-02-11 13:52:36 +0200

    fix bugs

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './slices/authSlice';
 import datepickerReducer from './slices/dateSlice';
-import tourReducer from './slices/tourByIdSlice';
 import tourPanelReducer from './slices/panelSlice';
 import paymentReducer from './slices/paymentSlice';
 import { tourApi } from './services/tourApi';
